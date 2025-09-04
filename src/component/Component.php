826d071d5007cfab9be4ef06bdf50baf5d53ad76
Ives <ivesdebanta@gmail.com>
2025-09-04 19:39:48 +0100

    Adding of primary functions on component class

--- src/component/Component.php
+++ src/component/Component.php
@@ -4,14 +4,26 @@
          * Class reserved to basic component visible on the website
          */
 
+        protected string $name;
         protected string $id;
         protected array $classes;
         protected array $attributes;
         protected string $blockName;  
-        public function __construct(array $class = ['component'], array $attributes = [], string $blockName) {
+        protected array $elements;
+        protected array $modifiers;
+        protected bool $visible;
+        protected bool $workable;
+        protected array $childs;
+        protected Component $parent;
+
+        public function __construct(string $blockName, array $class = ['component'], array $attributes = []) {
             $this->classes = $class;
             $this->attributes = $attributes;
             $this->blockName = $blockName;
+            $this->elements = [];
+            $this->modifiers = [];
+            $this->workable = true;
+            $this->visible = true;
         }
         
         /**
@@ -49,12 +61,126 @@
             $this->attributes = $newAttributes;
         }
 
+        /**
+         * Function used to add attributes to current component
+         * @param array $addedAttributes
+         * @return void
+         */
         public function addAttributes(array $addedAttributes){
             array_push($this->attributes, $addedAttributes);
         }
 
+        /**
+         * Function used to add a block to the BEM of current component
+         * @param string $newBlockName
+         * @return void
+         */
         public function setBlockName(string $newBlockName){
             $this->blockName = $newBlockName;
         }
 
+        /**
+         * Function used to add modifier to block on BEM. It returns a boolean depends of
+         * the presence of element in array of elements and presence of the modifier to set in
+         * the modifier array
+         * @param string $modifierName
+         * @param string $elementLinked
+         * @return bool
+         */
+        public function addModifier(string $modifierName, string $elementLinked) {
+            if(in_array($elementLinked, $this->elements) and !in_array($elementLinked.'--'.$modifierName, $this->modifiers)){
+                array_push($this->modifiers, $elementLinked.'--'.$modifierName);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        /**
+         * Function used to add element to block on BEM. It returns a boolean depends of
+         * the presence of element in array of elements
+         * @param string $elementName
+         * @return bool
+         */
+        public function addElement(string $elementName) : bool{
+            if(!in_array($elementName, $this->elements)){
+                array_push($this->elements, $this->blockName.'__'.$elementName);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        /**
+         * Function used to add a parent to the current component
+         * @param Component $parent
+         * @return void
+         */
+        public function setParent(?Component $parent){
+            $this->parent = $parent;
+        }
+
+        /**
+         * Function used to get the parent of the current component
+         * @return Component
+         */
+        public function getParent(): Component{
+            return $this->parent;
+        }
+
+        /**
+         * Function used to set some child to the current component
+         * @param array $childs
+         * @return void
+         */
+        public function setChilds(array $childs){
+            $this->childs = $childs;
+        }
+
+        /**
+         * Function used to add child to the current component
+         * @param Component $child
+         * @return void
+         */
+        public function addChild(Component $child){
+            array_push( $this->childs, $child);
+        }
+
+        /**
+         * Function used to get all the childs of the current component
+         * @return array
+         */
+        public function getChild(): array{
+            return $this->childs;
+        }
+
+        public function setName(string $newName){
+            $this->name = $newName;
+        }
+
+        public function getName(): string{
+            return $this->name;
+        }
+        // /**
+        //  * Function used to make visible the current component
+        //  * @return void
+        //  */
+        // abstract public function render();
+
+        // /**
+        //  * Function used to make the current component not workable
+        //  * @return void
+        //  */
+        // abstract public function disable();
+        // /**
+        //  * Function used to make the current component workable
+        //  * @return void
+        //  */
+        // abstract public function enable();
+
+        // /**
+        //  * Function used to hide the current component
+        //  * @return void
+        //  */
+        abstract public function hide();
     }
--- tests/ComponentTest.php
+++ tests/ComponentTest.php
@@ -1,2 +1,30 @@
 <?php
     require_once __DIR__.'/../src/component/Component.php';
+    class ComponentTest extends Component{
+        public function __construct() {
+            parent::__construct('testBlock');
+        }
+        public function render(){
+            ?>
+            <div class=<?=$this->blockName?>>
+                <?=$this->blockName?>
+            </div>  
+            <?php
+        }
+        public function hide(){
+            if ($this->visible){
+                ?>
+                <style>
+                    <?='.'.$this->blockName?>{
+                        display : none;
+                    }
+                </style>
+                <?php
+            } else {
+                return;
+            }
+        }
+    }
+    $comp = new ComponentTest;
+    $comp->render();
+    $comp->hide();
