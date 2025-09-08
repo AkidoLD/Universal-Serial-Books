d6230536d66a22b57cc7dbbf53847491fa3bfe75
AkidoLD <alexrayan14231423@gmail.com>
2025-09-08 21:39:10 +0100

    Add Repository and Service directory

--- public/index.php
+++ public/index.php
@@ -1,5 +1,15 @@
 <?php
 
-require_once __DIR__."/../tests/PersonTest.php";
-require_once __DIR__."/../tests/BookTest.php";
-require_once __DIR__."/../tests/UserTest.php";
+// require_once __DIR__."/../tests/PersonTest.php";
+// require_once __DIR__."/../tests/BookTest.php";
+// require_once __DIR__."/../tests/UserTest.php";
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Ramsey\Uuid\Uuid;
+
+$uuid = Uuid::uuid4()->toString();
+$uuid2 = Uuid::uuid4()->toString();
+
+echo "Mon UUID : " . $uuid;
+echo "Mon UUID2: ".$uuid2;
