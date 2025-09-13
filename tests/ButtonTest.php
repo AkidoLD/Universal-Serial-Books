f92f78290187a2399c5bb68cbc347de578866757
Ives <ivesdebanta@gmail.com>
2025-09-13 18:04:36 +0100

    Setting of style on button

--- tests/ButtonTest.php
+++ tests/ButtonTest.php
@@ -1,18 +1,11 @@
 <?php
+    require __DIR__.'/../vendor/autoload.php';
     use App\View\Components\Button;
     /**
      * ********** Test step **********
      */
     $test = new Button('Button for test');
-    $test->round(8);
-    $test->setBColor('green');
-    $test->setTColor('white');
     $test->render();
-    $test->disable();
-    $test->setBorder('red', 2);
-    $test->disable();
-    $test->enable();
-    $test->setSize(100, 200);
     /**
      * ********** End of test **********
      */
