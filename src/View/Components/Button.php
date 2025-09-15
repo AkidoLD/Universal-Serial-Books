3d28a345f49653b3636fd727f53cd3faaa064fa2
Ives <ivesdebanta@gmail.com>
2025-09-15 18:40:26 +0100

    Case not always ignored

---
+++ src/View/Components/Button.php
@@ -0,0 +1,44 @@
+<?php
+    namespace App\View\Components;
+    use App\View\Components\Component;
+    /**
+     * Class used to represent a button
+     */
+    class Button extends Component{
+        private string $text;
+        private array $style;
+
+        /**
+         * Constructor of a button component
+         * @param string $textContent
+         */
+        public function __construct(string $textContent, string  $blockName = 'button') {
+            parent::__construct($blockName, ['button']);
+            $this->text = $textContent;
+            $this->style = [];
+        }
+
+        /**
+         * Function used to hide a button
+         * @return void
+         */
+        public function hide(){
+            ?>
+            <style>
+                <?='.'.$this->blockName?>{
+                    display: none;
+                }
+            </style>
+            <?php
+        }
+        /**
+         * Function used to render a button
+         * @return void
+         */
+        public function render(){
+            ?>
+            <input type="button" class="<?=implode('',$this->getClasses())?>" value="<?=$this->text?>">
+            >
+            <?php
+        }
+    }
