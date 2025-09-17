1a6b67670429c955153169ce10fb1eb93108c670
Ives <ivesdebanta@gmail.com>
2025-09-17 17:25:25 +0100

    File for enumarate icons

---
+++ src/Enums/IconLoader.php
@@ -0,0 +1,15 @@
+<?php
+    namespace App\Enums;
+    enum IconLoader: string{
+        case BLACK_BOOK = "/Universal-Serial-Books/public/assets/imgs/black book.png";
+        case BOOK = "/Universal-Serial-Books/public/assets/imgs/book.png";
+        case COMMENT = "/Universal-Serial-Books/public/assets/imgs/comment";
+        case EMPTY_STAR = "/Universal-Serial-Books/public/assets/imgs/empty star.png";
+        case fleche = "/Universal-Serial-Books/public/assets/imgs/fleche.png";
+        case HEART = "/Universal-Serial-Books/public/assets/imgs/heart.png";
+        case HOME = "/Universal-Serial-Books/public/assets/imgs/home.png";
+        case QUESTION = "/Universal-Serial-Books/public/assets/imgs/question.png";
+        case SETTINGS = "/Universal-Serial-Books/public/assets/imgs/settings.png";
+        case USB = "/Universal-Serial-Books/public/assets/imgs/usb.png";
+        case STAR = "/Universal-Serial-Books/public/assets/imgs/star.png";
+    }
